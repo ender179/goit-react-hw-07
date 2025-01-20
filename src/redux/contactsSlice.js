99000276572815f7b626c2502c0ea9702b2b2018
ender179 <kirilukvlad91@gmail.com>
2025-01-20 22:29:20 +0200

    git push

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,59 +1,41 @@
 import { createSlice, isAnyOf } from '@reduxjs/toolkit';
-// Імпорт асинхронних Thunk-дій fetchContacts, addContacts, deleteContacts з файлу './operations'
 import { fetchContacts, addContacts, deleteContacts } from './operations';
 
-// Визначення масиву extraActions, що містить асинхронні Thunk-дії
-// const extraActions = [fetchContacts, addContacts, deleteContacts];
-
-// Визначення функції getActions, яка повертає умову isAnyOf для зазначеного типу дії
-// const getActions = type => isAnyOf(...extraActions.map(action => action[type]));
-
-// Визначення функції getActions, яка повертає умову isAnyOf для зазначеного типу дії
 const getActions = type =>
   isAnyOf(fetchContacts[type], addContacts[type], deleteContacts[type]);
 
-// Початковий стан для slice contactsSlice
 const initialState = { items: [], isLoading: false, error: null };
 
-// Створення slice для керування контактами
 const contactsSlice = createSlice({
-  name: 'contacts', // Унікальне ім'я для slice
-  initialState, // Початковий стан slice
+  name: 'contacts', 
+  initialState, 
   extraReducers: builder =>
     builder
       .addCase(fetchContacts.fulfilled, (state, action) => {
-        // Обробка успішного виконання fetchContacts
-        state.items = action.payload; // Оновлення списку контактів у стані
+        state.items = action.payload; 
       })
       .addCase(addContacts.fulfilled, (state, action) => {
-        // Обробка успішного виконання addContacts
-        state.items.unshift(action.payload); // Додавання нового контакту на початок списку контактів може
+        state.items.unshift(action.payload); 
       })
       .addCase(deleteContacts.fulfilled, (state, action) => {
-        // Обробка успішного виконання deleteContacts
         const index = state.items.findIndex(
           contact => contact.id === action.payload.id
         );
-        state.items.splice(index, 1); // Видалення контакту зі списку контактів
+        state.items.splice(index, 1); 
       })
       .addMatcher(getActions('pending'), state => {
-        // Обробка дій зі статусом 'pending' очікування
-        state.isLoading = true; // Установка прапора isLoading true
+        state.isLoading = true; 
       })
       .addMatcher(getActions('rejected'), (state, action) => {
-        // Обробка дій зі статусом 'rejected' відхилено
-        state.isLoading = false; // Скидання прапора isLoading false
-        state.error = action.payload; // Встановлення повідомлення про помилку
+        state.isLoading = false; 
+        state.error = action.payload; 
       })
       .addMatcher(getActions('fulfilled'), state => {
-        // Обробка дій зі статусом 'fulfilled' виконано
-        state.isLoading = false; // Скидання прапора isLoading false
-        state.error = null; // Скидання повідомлення про помилку null
+        state.isLoading = false; 
+        state.error = null; 
       }),
 });
 
-// Експорт дій addContact і deleteContact з slice contactsSlice
 export const { addContact, deleteContact } = contactsSlice.actions;
 
-// Експорт редуктора (reducer) contactsReducer з slice contactsSlice
 export const contactsReducer = contactsSlice.reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -10,14 +10,12 @@ import {
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
 
-// Створення сховища Redux за допомогою configureStore
 export const store = configureStore({
   reducer: {
-    contacts: contactsReducer, // Редюсер для керування станом контактів
-    filter: filterReducer, // Редюсер для керування станом фільтра
+    contacts: contactsReducer, 
+    filter: filterReducer, 
   },
 
-  // Застосування middleware за допомогою getDefaultMiddleware
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
       serializableCheck: {
