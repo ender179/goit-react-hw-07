366733f7946e59cc3e1523c66203992b917c3fba
ender179 <kirilukvlad91@gmail.com>
2025-01-21 23:42:40 +0200

    git push

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,14 +1,12 @@
-import { createSelector } from '@reduxjs/toolkit';
+export const selectFilter = state => state.filter.name;  
 
-const selectContacts = state => state.contacts.items;
-const selectFilter = state => state.filters.name;
+export const selectContacts = state => state.contacts;  
 
-export const selectFilteredContacts = createSelector(
-  [selectContacts, selectFilter],
-  (contacts, filter) => {
-    const normalizedFilter = filter.toLowerCase();
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().includes(normalizedFilter),
-    );
-  }
-);
+export const selectFilteredContacts = state => {  
+  const allContacts = selectContacts(state);  
+  const filter = selectFilter(state);  
+
+  return allContacts.filter(contact =>  
+    contact.name.toLowerCase().includes(filter.toLowerCase())  
+  );  
+};
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,12 @@
-// src/redux/store.js  
 import { configureStore } from '@reduxjs/toolkit';  
 import contactsReducer from './contactsSlice';  
-import filterReducer from './filterSlice'; 
+import filterReducer from './filterSlice';  
 
 const store = configureStore({  
-    reducer: {  
-        contacts: contactsReducer,  
-        filter: filterReducer 
-    },  
+  reducer: {  
+    contacts: contactsReducer,  
+    filter: filterReducer,  
+  },  
 });  
 
 export default store;
