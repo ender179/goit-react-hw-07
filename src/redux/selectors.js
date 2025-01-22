9d91cb9e4296077ad2d223383cfb9d58412efc4b
ender179 <kirilukvlad91@gmail.com>
2025-01-22 07:37:22 +0200

    git push

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,12 +1,17 @@
+import { createSelector } from 'reselect';  
+
+// Селектор для отримання елементів контактів  
+export const selectContacts = state => state.contacts.items;  
+
+// Селектор для отримання імені фільтра  
 export const selectFilter = state => state.filter.name;  
 
-export const selectContacts = state => state.contacts;  
-
-export const selectFilteredContacts = state => {  
-  const allContacts = selectContacts(state);  
-  const filter = selectFilter(state);  
-
-  return allContacts.filter(contact =>  
-    contact.name.toLowerCase().includes(filter.toLowerCase())  
-  );  
-};
+// Селектор для отримання відфільтрованих контактів  
+export const selectFilteredContacts = createSelector(  
+  [selectContacts, selectFilter],  
+  (allContacts, filter) => {  
+    return allContacts.filter(contact =>  
+      contact.name.toLowerCase().includes(filter.toLowerCase())  
+    );  
+  }  
+);
